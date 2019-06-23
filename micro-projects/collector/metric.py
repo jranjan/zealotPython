0167c1cfc93dd3e821da9ebc701bb3a680a11f38
jranjan <jranjan@gmail.com>
2019-06-23 21:01:13 +0530

    cleaning old bits

--- micro-projects/collector/metric.py
+++
@@ -1,8 +0,0 @@
-class Metric:
-    def __init__(self, name, produced_at, data=dict()):
-        self.name = name
-        self.produced_at = produced_at
-        self.info = data
-
-    def get(self):
-        return self.data

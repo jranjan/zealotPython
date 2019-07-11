645845b56225e5e0de3a2bf092e61ebecc7646f4
ranjanj <jyoti.ranjan@hpe.com>
2019-07-11 11:55:17 +0530

    plus one

--- leetcodes/plus_one.py
+++ leetcodes/plus_one.py
@@ -1,3 +1,17 @@
+# Given a non-empty array of digits representing a non-negative integer, plus one to the integer.
+#
+# The digits are stored such that the most significant digit is at the head of the list, and each
+# element in the array contain a single digit.
+#
+# You may assume the integer does not contain any leading zero, except the number 0 itself.
+#
+#
+# Example 1:
+#
+# Input: [1,2,3]
+# Output: [1,2,4]
+# Explanation: The array represents the integer 123.
+
 class Solution(object):
     def plusOne(self, digits):
         """
@@ -20,7 +34,3 @@ class Solution(object):
             digits.insert(0, 1)
 
         return digits
-
-if __name__ == "__main__":
-    s = Solution()
-    print(s.plusOne([8, 9,9,9]))
